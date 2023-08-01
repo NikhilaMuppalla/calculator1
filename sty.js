75e33d00f97a4a1cc1fb855cc73adc7c3bdc46d7
NikhilaMuppalla <100996695+NikhilaMuppalla@users.noreply.github.com>
2023-08-01 14:59:15 +0530

    Update sty.js

--- sty.js
+++ sty.js
@@ -12,120 +12,107 @@ let E = Math.E;
 // Function to remove data from the <p> element and localStorage
 const calculator = document.getElementById('calculator');
 const history = document.getElementById('history');
-const myLink = document.getElementById('myLink');
+const historyButton = document.getElementById('historyButton');
 const historyList = document.getElementById('historyList');
 const clearHistoryButton = document.getElementById('clearHistoryButton');
 
 // Function to perform the calculation and update the calculator history
-function handleLinkClick(event) {
-    // Prevent the default navigation behavior
-    event.preventDefault();
+function performCalculation(expression) {
+  // Retrieve the current calculator history from LocalStorage
+  const calculatorHistory = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
 
-    // Display a message in the console
-    console.log('Link Clicked!');
-    showHistoryView();
-    window.location.href = 'history.html';
-}
+  // Add the calculation expression and result to the history
+  calculatorHistory.push(`${expression}`);
 
-
-
-function performCalculation(data) {
-    // Retrieve the current calculator history from LocalStorage
-    const calculatorHistory = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
-
-    // Add the calculation expression and result to the history
-    calculatorHistory.push(`${data}`);
-
-    // Update LocalStorage with the updated calculator history
-    localStorage.setItem('calculatorHistory', JSON.stringify(calculatorHistory));
+  // Update LocalStorage with the updated calculator history
+  localStorage.setItem('calculatorHistory', JSON.stringify(calculatorHistory));
 }
 
 // Function to display the calculator view
 function showCalculatorView() {
-    calculator.style.display = 'block';
-    history.style.display = 'none';
+  calculator.style.display = 'block';
+  history.style.display = 'none';
 }
 
 // Function to display the history view
 function showHistoryView() {
-    calculator.style.display = 'none';
-    history.style.display = 'block';
+  calculator.style.display = 'none';
+  history.style.display = 'block';
 
-    // Display the calculator history when the history view is shown
-    displayHistory();
+  // Display the calculator history when the history view is shown
+  displayHistory();
 }
 
 // Function to display the calculator history
 function displayHistory() {
-    // Retrieve the calculator history from LocalStorage
-    const calculatorHistory = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
+  // Retrieve the calculator history from LocalStorage
+  const calculatorHistory = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
 
-    // Clear existing history entries from the list
-    historyList.innerHTML = '';
+  // Clear existing history entries from the list
+  historyList.innerHTML = '';
 
-    // Loop through the calculatorHistory array and display each entry in the list
-    calculatorHistory.forEach(entry => {
-        const listItem = document.createElement('li');
-        listItem.textContent = entry;
+  // Loop through the calculatorHistory array and display each entry in the list
+  calculatorHistory.forEach(entry => {
+    const listItem = document.createElement('li');
+    listItem.textContent = entry;
 
-        // Create a delete button for this entry
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener('click', () => deleteEntry(entry));
+    // Create a delete button for this entry
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => deleteEntry(entry));
 
-        listItem.appendChild(deleteButton);
-        historyList.appendChild(listItem);
-    });
+    listItem.appendChild(deleteButton);
+    historyList.appendChild(listItem);
+  });
 }
 
 // Function to delete a calculator history entry
 function deleteEntry(entry) {
-    // Retrieve the calculator history from LocalStorage
-    const calculatorHistory = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
+  // Retrieve the calculator history from LocalStorage
+  const calculatorHistory = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
 
-    // Filter out the entry to be deleted
-    const newCalculatorHistory = calculatorHistory.filter(item => item !== entry);
+  // Filter out the entry to be deleted
+  const newCalculatorHistory = calculatorHistory.filter(item => item !== entry);
 
-    // Update LocalStorage with the new data
-    localStorage.setItem('calculatorHistory', JSON.stringify(newCalculatorHistory));
+  // Update LocalStorage with the new data
+  localStorage.setItem('calculatorHistory', JSON.stringify(newCalculatorHistory));
 
-    // Refresh the displayed calculator history after deletion
-    displayHistory();
+  // Refresh the displayed calculator history after deletion
+  displayHistory();
 }
 
 // Function to clear the entire calculator history
 function clearHistory() {
-    // Update LocalStorage with an empty array
-    localStorage.clear();
+  // Update LocalStorage with an empty array
+  localStorage.setItem('calculatorHistory', JSON.stringify([]));
 
-    // Refresh the displayed calculator history after clearing
-    displayHistory();
+  // Refresh the displayed calculator history after clearing
+  displayHistory();
 }
 
 // When the user clicks the History button, show the history view
 historyButton.addEventListener('click', () => {
-
+  showHistoryView();
 });
 
 // When the user clicks the "Back to Calculator" button in the history view, show the calculator view
 const backButton = document.createElement('button');
 backButton.textContent = 'Back to Calculator';
 backButton.addEventListener('click', () => {
-    showCalculatorView();
-});
-clearHistoryButton.addEventListener('click', () => {
-    showCalculatorView();
+  showCalculatorView();
 });
 history.appendChild(backButton);
 
 // Initial setup when the page loads
 function initializeApp() {
-    // Set up calculator UI and logic here
-    // ...
+  // Set up calculator UI and logic here
+  // ...
 
-    // Display the calculator view initially
-    showCalculatorView();
+  // Display the calculator view initially
+  showCalculatorView();
 }
+
+
 
 
 
